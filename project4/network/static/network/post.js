c5c409cd14d45fac4f233479fb803d442b06e6ff
Chris Hui <christopher.slhui@gmail.com>
2021-04-29 09:27:38 +0100

    Update JavaScript

--- project4/network/static/network/post.js
+++ project4/network/static/network/post.js
@@ -1,37 +1,27 @@
-document.addEventLister('DOMContentLoaded', function() {
-
-  document.querySelector('#button-test').onclick = count;
+document.addEventListener('DOMContentLoaded', function() {
+  // Test
+  document.querySelector('#test-count').onclick = count;
 
   // Hide editor by default?
   // document.querySelector('#editor-view').style.display = 'none';
 
   // Use button to toggle post view
+  // TODO
   document.querySelector('#editor').addEventListener('click', () => load_editor());
-
-  document.querySelector('#test').addEventListener('click', () => test());
-
-  test();
-
-
-
-
 
 });
 
+// Test
+let counter = 0;
+function count() {
+  counter++;
+  document.querySelector('#test').innerHTML = counter;
+}
+
 // Post editor
+// TODO
 function load_editor() {
   document.querySelector('#editor-view').style.display = 'block';
-  document.querySelector('#editor-view').innerHTML = `<h3>test</h3>`;
 }
 
-// Test
-function test() {
-  document.querySelector('#editor-view').style.display = 'none';
-  document.querySelector('#emails-view').innerHTML = `<h3>Test!!</h3>`;
-}
-
-let counter = 0
-function count() {
-  counter ++;
-  document.querySelector('#counter-test').innerHTML = counter;
-}
+// Display posts
