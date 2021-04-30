b04d974c38b743e50905272784c3ac9d68bd2b27
Chris Hui <christopher.slhui@gmail.com>
2021-04-30 09:23:19 +0100

    update post

--- project4/network/static/network/post.js
+++ project4/network/static/network/post.js
@@ -2,6 +2,7 @@
 function edit(post_id) {
   var edit_box = document.querySelector(`#edit-box-${post_id}`);
   var edit_btn = document.querySelector(`#edit-btn-${post_id}`);
+  // Show edit box and button
   edit_box.style.display = 'block';
   edit_btn.style.display = 'block';
 
@@ -13,9 +14,10 @@ function edit(post_id) {
         content: edit_box.value
       })
     })
+    // Hide edit box and button
     edit_box.style.display = 'none';
     edit_btn.style.display = 'none';
-
+    // Update content section of edited post
     document.querySelector(`#post-${post_id}`).innerHTML = edit_box.value;
 
   });
