9642e4f1b920118c6ff2ddc0e8ad5873f1dd9c08
Chris Hui <christopher.slhui@gmail.com>
2021-05-01 11:25:28 +0100

    update like/unlike models

--- project4/network/static/network/post.js
+++ project4/network/static/network/post.js
@@ -23,53 +23,19 @@ function edit(post_id) {
   });
 }
 
-// Like/unlike posts
+// Like/unlike posts 
 function like(post_id) {
   var like_btn = document.querySelector(`#like-${post_id}`);
 
   // POST request
-  like_btn.addEventListener('click', () => {
-    fetch(`/like/${post_id}`,{
-      method: 'POST'
-    })
-    .then(response => response.json())
-    .then(result => {
-      console.log(result);
-      // Change button from 'like' to 'unlike'
-      document.querySelector(`#like-${post_id}`).innerHTML = 'Unlike';
-    })
+  fetch(`/like/${post_id}`, {
+    method: 'POST'
+  })
+  .then(response => response.json())
+  .then(result => {
+    console.log(result);
+    // Change button from 'like' to 'unlike'
+    document.querySelector(`#like-${post_id}`).innerHTML = 'Unlike';
+  })
 
-
-  });
 }
-
-
-
-
-
-
-
-
-// Like or unlike posts
-// function like(post_id) {
-//   var like_btn = document.querySelector(`#like-btn-${post_id}`);
-//
-//   // POST request
-//   // Like/Unlike post
-//   like_btn.addEventListener('click', () => {
-//     fetch(`/liked/${post_id}`,{
-//       method: 'POST',
-//     })
-//     .then(repsonse => response.json())
-//     .then(result => {
-//       console.log(result);
-//       // Post is liked, change button to 'unlike'
-//       if ('li' in result) {
-//         document.querySelector(`#like-btn-${post_id}`).innerHTML = 'Unlike';
-//       }
-//       else if ('ul' in result) {
-//         document.querySelector(`#unlike-btn-${post_id}`).innerHTML = 'like';
-//       }
-//     })
-//   });
-// }
